2f62461fc0fbd970cae4012dd4f248c7dfbcd258
kubaadamczyklogin <poczta@kubaadamczyk.pl>
2022-09-06 15:23:49 +0200

    Aktualizacja rozkladu  04.09.2022

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ const typeDefs = gql`
 
 const trainsToTorun = [
   {
-    BydBie: "13:45",
-    TorGlo: "14:24",
-    TorMia: "14:28"
+    BydBie: "13:47",
+    TorGlo: "14:26",
+    TorMia: "14:31"
   },
   {
-    BydBie: "14:03",
-    TorGlo: "14:33"
+    BydBie: "14:08",
+    TorGlo: "14:38"
   },
   {
     BydBie: "14:41",
@@ -44,72 +44,72 @@ const trainsToTorun = [
     TorMia: "15:28"
   },
   {
-    BydBie: "15:11",
-    TorGlo: "15:41"
+    BydBie: "15:15",
+    TorGlo: "15:45"
   },
   {
     BydBie: "15:39",
-    TorGlo: "16:16",
-    TorMia: "16:21"
+    TorGlo: "16:18",
+    TorMia: "16:23"
   },
   {
-    BydBie: "16:19",
-    TorGlo: "16:52"
+    BydBie: "16:00",
+    TorGlo: "16:33"
   },
   {
-    BydBie: "16:50",
-    TorGlo: "17:29"
+    BydBie: "16:57",
+    TorGlo: "17:36"
   },
   {
-    BydBie: "17:51",
-    TorGlo: "18:30"
+    BydBie: "17:49",
+    TorGlo: "18:28"
   },
   {
-    BydBie: "18:13",
-    TorGlo: "18:44"
+    BydBie: "18:19",
+    TorGlo: "18:50"
   },
   {
-    BydBie: "18:54",
-    TorGlo: "19:33",
-    TorMia: "19:40"
+    BydBie: "18:48",
+    TorGlo: "19:27",
+    TorMia: "19:33"
   },
   {
-    BydBie: "19:46",
-    TorGlo: "20:25",
-    TorMia: "20:29"
+    BydBie: "19:43",
+    TorGlo: "20:22",
+    TorMia: "20:34"
   },
   {
-    BydBie: "21:21",
-    TorGlo: "21:51"
+    BydBie: "21:22",
+    TorGlo: "21:52"
   },
   {
-    BydBie: "23:06",
-    TorGlo: "23:45"
+    BydBie: "22:42",
+    TorGlo: "23:21"
   }
 ];
 
 const trainsToBydgoszcz = [
   {
-    TorGlo: "4:14",
-    BydBie: "4:52"
+    TorGlo: "4:24",
+    BydBie: "5:01"
   },
   {
-    TorGlo: "5:01",
-    BydBie: "5:31"
+    TorGlo: "4:57",
+    BydBie: "5:27"
   },
   {
-    TorMia: "5:24",
-    TorGlo: "5:29",
-    BydBie: "6:06"
+    TorMia: "5:19",
+    TorGlo: "5:26",
+    BydBie: "6:03"
   },
   {
-    TorGlo: "6:10",
-    BydBie: "6:39"
+    TorGlo: "6:17",
+    BydBie: "6:46"
   },
   {
     TorMia: "6:27",
-    TorGlo: "6:34",
-    BydBie: "7:11"
+    TorGlo: "6:33",
+    BydBie: "7:10"
   },
   {
     TorGlo: "7:21",
@@ -117,31 +117,35 @@ const trainsToBydgoszcz = [
   },
   {
     TorGlo: "7:39",
-    BydBie: "8:15"
+    BydBie: "8:14"
   },
   {
-    TorGlo: "8:19",
-    BydBie: "8:48"
+    TorGlo: "8:15",
+    BydBie: "8:44"
   },
   {
-    TorGlo: "8:53",
-    BydBie: "9:29"
+    TorGlo: "8:46",
+    BydBie: "9:21"
   },
   {
-    TorGlo: "9:36",
-    BydBie: "10:13"
+    TorGlo: "9:35",
+    BydBie: "10:11"
   },
   {
-    TorGlo: "10:50",
-    BydBie: "11:19"
+    TorGlo: "10:17",
+    BydBie: "10:46"
   },
   {
-    TorGlo: "12:49",
-    BydBie: "13:19"
+    TorGlo: "10:34",
+    BydBie: "11:09"
   },
   {
-    TorGlo: "13:28",
-    BydBie: "14:07"
+    TorGlo: "12:40",
+    BydBie: "13:12"
+  },
+  {
+    TorGlo: "13:34",
+    BydBie: "14:12"
   }
 ];
 
